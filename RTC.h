408a8ccd01d6bc2c6e1423d71c757bd41c2d9760
ThomHPL <thomas.herpoel@gmail.com>
2015-12-16 01:05:29 +0100

    added missing rtc files

---
+++ RTC.h
@@ -0,0 +1,50 @@
+/*
+ * RTC.h
+ *
+ * Created: 15-12-15 13:48:55
+ *  Author: Antoine
+ */ 
+// DS1307
+
+
+#ifndef RTC_H_
+#define RTC_H_
+
+#define	DS1307_ADDR	0b1101000
+
+#endif /* RTC_H_ */
+/*
+#define		SECONDS			rtc_register[0]
+#define		MINUTES			rtc_register[1]
+#define		HOURS			rtc_register[2]
+#define		DAY    			rtc_register[3]
+#define		DATE   			rtc_register[4]
+#define		MONTH  			rtc_register[5]
+#define		YEAR   			rtc_register[6]
+
+
+char rtc_register[7];
+char time[8]; 		//xx:xx:xx;
+char date[10];		//xx/xx/xxxx;
+char day;
+
+
+void RTC_setStartAddress(void);
+
+unsigned char RTC_read(void);
+
+void RTC_getTime(void);
+
+void RTC_getDate(void);
+
+char RTC_displayTime(char);
+
+void RTC_updateRegisters(void);
+
+unsigned char RTC_writeTime(void);
+
+char RTC_updateTime(void);
+
+void RTC_updateDate(void);
+
+*/
